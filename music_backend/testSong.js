bed9ed1c6772bf56764a3a6ed63416ddded5ca99
JambuluruNavyaSree <navyasree@example.com>
2026-03-16 15:37:10 +0530

    Full project replacement - clean UI, backend consolidation, excluded modules

--- music_backend/testSong.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Song = require('./models/Song');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const songs = await Song.find({songName: /bagari/i}).populate('artistId');
-  console.log(JSON.stringify(songs, null, 2));
-  process.exit(0);
-});
